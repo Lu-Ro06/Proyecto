4432f7e7110306e8343b2303da049c28f14e3739
Maria Lucero Rodea Martinez <160086366+Lu-Ro06@users.noreply.github.com>
2025-03-17 20:22:53 -0600

    Add files via upload

---
+++ page.js
@@ -0,0 +1,9 @@
+import ReservationForm from "@/components/ReservationForm";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-gray-100">
+      <ReservationForm />
+    </main>
+  );
+}
